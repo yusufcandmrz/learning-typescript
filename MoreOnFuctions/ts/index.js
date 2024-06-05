ebff262baf6a4489670d5f30d45472ceb3deb4eb
Yusuf Demiroz 00858166 <yusufcandmrz@gmail.com>
2024-06-05 16:47:54 +0300

    Updated MoreOnFunctions

--- MoreOnFuctions/ts/index.js
+++ MoreOnFuctions/ts/index.js
@@ -21,3 +21,43 @@ doSomething(myFunc);
 function fn(ctor) {
     return new ctor("hello");
 }
+console.log("*******************");
+// Generic Functions
+function firstElement(arr) {
+    return arr[0];
+}
+const element = firstElement(["1", "2", "3"]);
+console.log(element);
+function map(arr, func) {
+    return arr.map(func);
+}
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
+console.log(parsed);
+function longest(a, b) {
+    return a.length > b.length ? a : b;
+}
+console.log(longest({ length: 12 }, { length: 24 }));
+console.log(longest([1, 2], [1, 2, 3]));
+console.log(longest("Yusuf", "Demiroz"));
+function combine(arr1, arr2) {
+    return arr1.concat(arr2);
+}
+const newArray = combine([1, 2, 3], [`4`, `5`, `6`]);
+console.log(newArray);
+function firstElement1(arr) {
+    return arr[0];
+}
+function firstElement2(arr) {
+    return arr[0];
+}
+console.log(typeof firstElement1([1, 2, 3]));
+console.log(typeof firstElement2([1, 2, 3]));
+function filter1(arr, func) {
+    return arr.filter(func);
+}
+function filter2(arr, func) {
+    return arr.filter(func);
+}
+function greet(s) {
+    console.log(`hello ${s}`);
+}
--- MoreOnFuctions/ts/index.ts
+++ MoreOnFuctions/ts/index.ts
@@ -46,3 +46,53 @@ interface CallOrConstruct {
     (n?: number): string,
     new(s: string): Date,
 }
+
+console.log("*******************");
+
+// Generic Functions
+
+function firstElement<Type>(arr: any[]): Type | undefined {
+    return arr[0];
+}
+const element = firstElement(["1", "2", "3"]);
+console.log(element);
+
+function map<Input, Output>(arr: Input[], func: (arg: Input) => Output): Output[] {
+    return arr.map(func);
+}
+const parsed = map(["1", "2", "3"], (n) => parseInt(n));
+console.log(parsed);
+
+function longest<Type extends { length: number }>(a: Type, b: Type): Type {
+    return a.length > b.length ? a : b;
+}
+console.log(longest({ length: 12 }, { length: 24 }));
+console.log(longest([1, 2], [1, 2, 3]));
+console.log(longest("Yusuf", "Demiroz"));
+
+function combine<Type>(arr1: Type[], arr2: Type[]): Type[] {
+    return arr1.concat(arr2);
+}
+const newArray = combine<string | number>([1, 2, 3], [`4`, `5`, `6`]);
+console.log(newArray);
+
+function firstElement1<Type>(arr: Type[]): Type {
+    return arr[0];
+}
+function firstElement2<Type extends any[]>(arr: Type) {
+    return arr[0];
+}
+console.log(typeof firstElement1([1, 2, 3]));
+console.log(typeof firstElement2([1, 2, 3]));
+
+function filter1<Type>(arr: Type[], func: (arg: Type) => boolean): Type[] {
+    return arr.filter(func);
+}
+function filter2<Type, Func extends (arg: Type) => boolean>(arr: Type[], func: Func): Type[] {
+    return arr.filter(func);
+}
+
+function greet<Str extends string>(s: Str) {
+    console.log(`hello ${s}`)
+}
+
